1c84c2d04ce007c1d40a63c805df285888d1b372
mathelab <ewy.mathe@osumc.edu>
2017-06-16 12:51:25 +0200

    Updated function to create a MultiDataSet object and make the code into an R package. It checks out with 2 notes currently

---
+++ IntLim/R/01_ReadData.R
@@ -0,0 +1,100 @@
+#' Read in CSV file
+#'
+#' The metadata associated with data files to be analyzed in IntLim is supplied
+#' as a CSV file with two columns and 6 rows: 
+#'    type,filenames
+#'    metabData,myfilename
+#'    geneData,myfilename
+#'    metabMetaData,myfilename (optional)
+#'    geneMetaData,myfilename (optional)
+#'    sampleMetaData,myfilename
+#'
+#' Note that all files supplied in the CSV file, and the CSV file itself should be placed in the same folder.  The software assumes will automatically retrieve the file path of
+#' the input files (based on location of CSV files).  
+#' Note also that the input data files should be in a specific format:
+#'	metabData: rows are metabolites, columns are samples
+#'	geneData: rows are genes, columns are samples
+#'	metabMetaData: rows are metabolites, features are columns
+#'	geneMetaData: rows are genes, features are columns
+#'	sampleMetaData: rows are samples, features are columns
+#' In addition, the first column of the sampleMetaData file is assumed to be the sample id, 
+#' and those sample ids should match the columns of metabData and geneData (e.g. it is required
+#' that all sample ids in the metabData and geneData are also in the sampleMetaDatafile).
+#'
+#'	
+#' @param inputFile input file in CSV format (see Despcription)
+#' @param metabid name of column from metabolite meta data to be used as id
+#'      (required, must match metabolite abundances data)
+#' @param geneid name of column from gene meta data to be used as id
+#'	(required, must match gene expression data)
+#' @param logmetab whether or not to log metabolite values (T/F)
+#' @param loggene whether or not to log gene values (T/F)
+#' @return MultiDataSet object with input data
+#'
+#' @examples
+#' \dontrun{
+#' dir <- system.file("extdata", package="IntLim", mustWork=TRUE)
+#' csvfile <- file.path(dir, "test.csv")
+#' mydata <- ReadData(csvfile,metabid='BIOCHEMICAL',geneid='X')
+#' }
+#' @export
+ReadData <- function(inputFile,metabid,geneid, logmetab=FALSE,loggene=FALSE){
+    # Check that file exists
+    if (!file.exists(inputFile)) {
+        stop("CSV input file does not exist")
+    }
+    # Make into df to make access easier
+    csvfile <- as.data.frame(utils::read.csv(inputFile, header=TRUE,row.names=1))
+
+    # Check column names are correct
+    if (colnames(csvfile)!="filenames") {
+        stop("Check column names of input files.  'type' and 'filenames' are required")
+    }
+   
+    # Check that all types required are present
+    mytypes <- c("metabData","geneData","metabMetaData","geneMetaData",
+         "sampleMetaData")
+    mymatches <- as.numeric(lapply(mytypes,function(x) 
+		length(which(rownames(csvfile)==x))))
+    if(sum(mymatches)!=5) {
+	stop("The column 'type' contains non-allowed entries (See Description)")}
+ 
+    # Check that files exist then read them in one by one
+    if(!file.exists(as.character(csvfile['metabData',]))) {
+	stop(paste("File", csvfile['metabData',], "does not exist"))} else {
+    MData<-utils::read.csv(as.character(csvfile['metabData',]),row.names = 1)}
+
+    if(!file.exists(as.character(csvfile['geneData',]))) {
+        stop(paste("File", csvfile['geneData',], "does not exist"))} else {
+    GData<-utils::read.csv(as.character(csvfile['geneData',]),row.names = 1)}
+
+    if(as.character(csvfile['metabMetaData',])=="") {
+	warning("No metadata provided for metabolites");MmetaData<-NULL; } else if
+    (!file.exists(as.character(csvfile['metabMetaData',]))) {
+        stop(paste("File", csvfile['metabMetaData',], "does not exist"))} else {
+    MmetaData<-utils::read.csv(as.character(csvfile['metabMetaData',]))}
+
+    if(as.character(csvfile['geneMetaData',])=="") {
+        warning("No metadta provided for genes");GmetaData<-NULL;} else if
+    (!file.exists(as.character(csvfile['geneMetaData',]))) {
+        stop(paste("File", csvfile['geneMetaData',], "does not exist"))} else {
+    GmetaData<-utils::read.csv(as.character(csvfile['geneMetaData',]))}
+
+    if(!file.exists(as.character(csvfile['sampleMetaData',]))) {
+        stop(paste("File", csvfile['sampleMetaData',], "does not exist"))} else {
+    pData<-utils::read.csv(as.character(csvfile['sampleMetaData',]),row.names = 1)}
+
+    #Create Multi
+    Mdata <- CreateMultiDataSet(genefdata=GmetaData, metabfdata=MmetaData,
+	metabid=metabid, geneid=geneid,pdata=pData,metabdata=MData,
+	genedata=GData,logmetab=logmetab,loggene=loggene)
+	
+	#genefdata=GmetaData; metabfdata=MmetaData;metabid="BIOCHEMICAL";geneid="X";
+	#pdata=pData;metabdata=MData;genedata=GData;logmetab=F;loggene=F
+
+    print("CreateMultiDataSet created")
+    return(MData)
+}
+
+
+
---
+++ IntLim/R/internalfunctions.R
@@ -0,0 +1,121 @@
+#' Generic function to create constructor of MultiDataSet gene object
+#'
+#' @param genefdata gene meta data
+#' @param metabfdata metabolite meta data
+#' @param pdata sample meta data
+#' @param geneid name of column from metabolite meta data to be used as id 
+#'	(required, must match metabolite abundances matrix))
+#' @param metabid name of column from gene meta data to be used as id
+#'      (required, must match gene abundances matrix))
+#' @param metabdata metabolite abundances (samples are in columns)
+#' @param genedata gene expression (samples are in columns)
+#' @param logmetab T/F 
+#' @param loggene T/F
+
+CreateMultiDataSet <- function(genefdata, metabfdata, pdata, geneid, metabid, 
+	metabdata, genedata, logmetab=FALSE,loggene=FALSE) {
+
+	# Check that feature data and abundance data metabolites corresponds
+        if(length(which(colnames(metabfdata)==metabid))!=1) {
+                stop(paste("metabid provided",metabid,"does not exist in metabolite meta data file"))} else if 
+	(length(intersect(rownames(metabdata),as.character(metabfdata[,metabid])))<nrow(metabdata)){
+                stop("Metabolites in abundance data file and metabolite meta data file are not equal")} else {
+                myind <- as.numeric(lapply(rownames(metabdata),function(x) {
+                        which(as.character(metabfdata[,metabid])==x)[1]}))
+                        metabpdata<-pdata[myind,]}
+
+	rownames(metabfdata)=as.character(metabfdata[,metabid])
+
+        # Check that samples data and abundance data samples correspond
+        if(length(intersect(colnames(metabdata),rownames(pdata)))<ncol(metabdata)){
+                stop("Samples in metabolite abundance data file and sample meta data file are not equal")
+        } else {
+                myind <- as.numeric(lapply(colnames(metabdata),function(x) {
+                        which(rownames(pdata)==x)[1]}))
+                        metabpdata<-pdata[myind,]
+        }
+
+	#new data frames are set for phenoData and featureData
+	metabpdata$id=rownames(metabpdata)
+	metabphenoData <- Biobase::AnnotatedDataFrame(data = metabpdata)
+	metabfeatureData <- Biobase::AnnotatedDataFrame(data = metabfdata)
+
+	if (logmetab == TRUE){
+		metabdata <- log2(metabdata)
+	}
+
+       methods::setClass (
+            Class = "MetaboliteSet",
+            contains = "eSet",
+            prototype = methods::prototype(methods::new("VersionedBiobase",
+                                      versions = c(Biobase::classVersion("eSet"),
+                                        MetaboliteSet = "1.0.0")))
+        )
+
+	metab.set <- methods::new("MetaboliteSet",metabData = metabdata, 
+		phenoData = metabphenoData, featureData = metabfeatureData)
+
+	#####  Now the genes
+	# Check that feature data and gene expression data corresponds
+       if(length(which(colnames(genefdata)==geneid))!=1) {
+                stop(paste("geneid provided",geneid,"does not exist in gene meta data file"))
+        } else if(length(intersect(rownames(genedata),as.character(genefdata[,geneid]))) < nrow(genedata)){
+                stop("Genes in expression data file and gene meta data file are not equal")
+        } else {
+                myind <- as.numeric(lapply(rownames(genedata),function(x) {
+                        which(as.character(genefdata[,geneid])==x)[1]}))
+                        genepdata<-pdata[myind,]
+        }
+
+        rownames(genefdata)=as.character(genefdata[,geneid])
+
+        # Check that samples data and abundance data samples correspond
+        if(length(intersect(colnames(genedata),rownames(pdata)))<ncol(genedata)){ 
+                stop("Samples in expression data file and sample meta data file are not equal")
+        } else {
+		myind <- as.numeric(lapply(colnames(genedata),function(x) {
+			which(rownames(pdata)==x)[1]}))
+			genepdata<-pdata[myind,]
+	}
+
+        #new data frames are set for phenoData and featureData
+        if (loggene == TRUE){
+                metabdata <- log2(genedata)
+        }
+
+        gene.set <- Biobase::ExpressionSet(assayData=as.matrix(genedata))
+	if(length(which(colnames(genefdata)=="chromosome"))==0) {
+		genefdata$chromosome <- rep("chr",nrow(genefdata))}
+	if(length(which(colnames(genefdata)=="start"))==0) {
+	        genefdata$start <- rep(0,nrow(genefdata))}
+	if(length(which(colnames(genefdata)=="end"))==0) {
+	        genefdata$end <- rep(0,nrow(genefdata))}
+	Biobase::fData(gene.set) <- genefdata
+	genepdata$id=rownames(genepdata)
+	Biobase::pData(gene.set) <- genepdata
+	
+
+	multi <- MultiDataSet::createMultiDataSet()
+	multi1 <- MultiDataSet::add_genexp(multi, gene.set)
+
+    methods::setGeneric("add_metabolite", function(object, metabSet, warnings = TRUE, ...)
+    base::standardGeneric("add_metabolite")
+    )
+
+    methods::setMethod(
+        f = "add_metabolite",
+        signature = c("MultiDataSet", "MetaboliteSet"),
+        definition = function(object, metabSet, warnings = TRUE, ...) {
+            ## Add given MetaboliteSet as 'metabolite'
+            object <- MultiDataSet::add_eset(object, metabSet, dataset.type = "metabolite", GRanges = NA, ...)
+            return(object)
+        })
+
+	multi2 <- add_metabolite(multi1, metab.set)
+
+	return(multi2)
+}
+
+
+
+
