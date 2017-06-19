8c78b5d9d3f3706246c4bcd373f834731323fa6c
mingrui-liu <liumingrui1997@gmail.com>
2017-06-19 14:46:42 -0400

    Add files via upload

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -1,1 +1,20 @@
+library(shiny)
 
+shinyServer(function(input, output) {
+    
+
+    output$contents <- renderPrint({
+        
+        
+        
+        inFile <- input$file1
+        if (is.null(inFile))
+            return(NULL)
+        
+        multiData<-ReadData(inFile$datapath,input$metabid,input$geneid)
+        multiData
+    })
+    
+        
+        
+})
--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -1,1 +1,28 @@
 
+library(shiny)
+library(MultiAssayExperiment)
+setwd("/Users/liumingrui/Documents/BIM/IntLiMpackage")
+source('/Users/liumingrui/Documents/BIM/IntLiMpackage/IntLiMModelcode.R')
+source('/Users/liumingrui/Documents/BIM/IntLiMpackage/Creat-MultiAssay-Obj.R')
+
+shinyUI(fluidPage(
+    titlePanel("Uploading Files"),
+    sidebarLayout(
+        sidebarPanel(
+            fileInput('file1', 'Choose CSV File',
+                      accept=c('text/csv', 
+                               'text/comma-separated-values,text/plain', 
+                               '.csv')),
+            textInput("metabid", "Metab ID", "BIOCHEMICAL"),
+            textInput("geneid", "Gene ID", "X")
+      
+        ),
+            
+        mainPanel(
+            pre(includeText("/Users/liumingrui/Documents/BIM/IntLim-master/IntLim/inst/extdata/textformat.txt")),
+            pre(textOutput('contents'))
+        )
+    )
+    )
+)
+
