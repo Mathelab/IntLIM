8a2665f7de04acac55f333ed2fb75b3668f73d79
mathelab <ewy.mathe@osumc.edu>
2017-06-21 10:57:31 +0200

    Integrated shiny app

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -1,20 +1,6 @@
-
-library(shiny)
-library(MultiDataSet)
-source('~/Documents/BIM/IntLim-master/IntLim/R/MetaboliteSet_addMetabolite.R')
-source('~/Documents/BIM/IntLim-master/IntLim/R/internalfunctions.R')
-source('~/Documents/BIM/IntLim-master/IntLim/R/AllClasses.R')
-source('~/Documents/BIM/IntLim-master/IntLim/R/01_ReadData.R')
-
-source('~/Documents/BIM/IntLiMpackage/IntLiMModelcode.R')
-source('/Users/liumingrui/Documents/BIM/Files_for_understanding_Data_and_Package/GEMfunctionsv3.R')
-
-setwd("/Users/liumingrui/Documents/BIM/IntLim-master/IntLim/inst/extdata")
-
-
 shinyUI(fluidPage(
     
-    titlePanel("Analyze metablon"),
+    titlePanel("IntLim (Integrate Metabolomics with other Omics data using linear modeling"),
     sidebarLayout(
         sidebarPanel(
             
@@ -27,31 +13,26 @@ shinyUI(fluidPage(
             textInput("metabid", "Metab ID", "BIOCHEMICAL"),
             textInput("geneid", "Gene ID", "X"),
             
-            
-         
-            
-                
-                checkboxGroupInput(inputId="groupA",
+            checkboxGroupInput(inputId="groupA",
                                    label="Pick Types for Group A", 
                                    choices=c("Normal"="N","Tumor"="T"), selected = "Tumor"),
                 
-                checkboxGroupInput(inputId="groupB",
+           checkboxGroupInput(inputId="groupB",
                                    label="Pick Types for Group B", 
                                    choices=c("Normal"="N","Tumor"="T"), selected = "Tumor")
             
-        ),
+        ), # end sidebarPanel
             
         mainPanel(
             tabsetPanel(
+                tabPanel('Data summary',
+                       pre(textOutput('contents'))
+                )
+             ) # end tabsetPanel
+               # tabPanel('Analyze By Group',pre(textOutput('group')))
                 
-                tabPanel('Data summary',pre(includeText("/Users/liumingrui/Documents/BIM/IntLim-master/IntLim/inst/extdata/textformat.txt"),
-                                            pre(textOutput('contents'))
-                                            )),
-                tabPanel('Analyze By Group',pre(textOutput('group')))
-                
-            )
-        )
-    )
-    )
-)
+            ) # end mainPanel
+        ) # end sidebarLayout
+    ) # end fluidPage
+) # end shinUI
 
