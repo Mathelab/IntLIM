8441136b9a1080893f46d71e04c7389a5f11a18a
mingrui-liu <liumingrui1997@gmail.com>
2017-07-17 10:32:03 -0400

    Add file input function

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -1,4 +1,26 @@
-
+# based on the Shiny fileInput function
+    fileInput2 <- function(inputId, label = NULL, labelIcon = NULL, multiple = FALSE,accept = NULL, width = NULL, progress = TRUE, ...) {
+        # add class fileinput_2 defined in UI to hide the inputTag
+        inputTag <- tags$input(id = inputId, name = inputId, type = "file", 
+                               class = "fileinput_2")
+        if (multiple) 
+            inputTag$attribs$multiple <- "multiple"
+        if (length(accept) > 0) 
+            inputTag$attribs$accept <- paste(accept, collapse = ",")
+        
+        div(..., style = if (!is.null(width)) paste0("width: ", validateCssUnit(width), ";"), 
+            inputTag,
+            # label customized with an action button
+            tags$label(`for` = inputId, div(icon(labelIcon), label, 
+                                            class = "btn btn-default action-button")),
+            # optionally display a progress bar
+            if(progress)
+                tags$div(id = paste(inputId, "_progress", sep = ""), 
+                         class = "progress shiny-file-input-progress", 
+                         tags$div(class = "progress-bar")
+                )
+        )
+    }          
 headerbar <- shinydashboard::dashboardHeader(
     title = "IntLim",
     titleWidth = 270,
