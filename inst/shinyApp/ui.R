d1b3fab2960beb8c18b407e8c645b79d009d0f77
mingrui-liu <liumingrui1997@gmail.com>
2017-07-18 11:02:43 -0400

    Update ui.R

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -218,7 +218,7 @@ body <- shinydashboard::dashboardBody(
                                      selected = "metabolite"),
                         hr(),
                         uiOutput('choosestype'),
-                        actionButton("run3", "Plot the adjusted p value distribution"),
+                        actionButton("run3", "Plot"),
                         
                         tags$head(tags$style(type="text/css", "
                         loadmessage {
