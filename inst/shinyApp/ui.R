634a22ea2550e40cf9349911e2a81162f88c9cb7
Shunchao Wang <wang.2799@osu.edu>
2018-08-23 14:48:22 -0400

    Added www to html path.

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -364,7 +364,7 @@ body <- shinydashboard::dashboardBody(
 
 
 shinyUI(fluidPage(
-  tags$head(includeHTML(("google_analytics.html"))),
+  tags$head(includeHTML(("www/google_analytics.html"))),
     shinydashboard::dashboardPage(
         headerbar,
         sidebar,
